6fe26938ac1039564540d08bbb09a02ebef65bc7
programmer10110 <programmer10110@yandex.ru>
2019-08-22 16:40:09 +0300

    lost code added

--- client/authtypes/keys.go
+++ client/authtypes/keys.go
@@ -3,4 +3,7 @@ package authtypes
 const (
 	// StoreKey is string representation of the store key for auth
 	StoreKey = "acc"
+
+	// QuerierRoute is the querier route for acc
+	QuerierRoute = StoreKey
 )
