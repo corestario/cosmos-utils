8d3cba387d62644f700fa9f324276f58af3e6db2
programmer10110 <programmer10110@yandex.ru>
2019-09-03 20:12:25 +0300

    new types in MakeCodec()

--- client/authtypes/codec.go
+++ client/authtypes/codec.go
@@ -8,8 +8,12 @@ import (
 // RegisterCodec registers concrete types on the codec
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterInterface((*exported.Account)(nil), nil)
+	cdc.RegisterConcrete(&BaseAccount{}, "cosmos-utils/Account", nil)
 	cdc.RegisterInterface((*exported.VestingAccount)(nil), nil)
-	cdc.RegisterConcrete(StdTx{}, "auth/StdTx", nil)
+	cdc.RegisterConcrete(&BaseVestingAccount{}, "cosmos-utils/BaseVestingAccount", nil)
+	cdc.RegisterConcrete(&ContinuousVestingAccount{}, "cosmos-utils/ContinuousVestingAccount", nil)
+	cdc.RegisterConcrete(&DelayedVestingAccount{}, "cosmos-utils/DelayedVestingAccount", nil)
+	cdc.RegisterConcrete(StdTx{}, "cosmos-utils/StdTx", nil)
 }
 
 // module wide codec
